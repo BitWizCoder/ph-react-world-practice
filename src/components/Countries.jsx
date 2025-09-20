04f3e36d53445d2c2778c19371b66f2da8faaa8e
Muhammad Noman <mhmdnoman01@gmail.com>
2025-09-20 16:10:39 +0600

    added styles

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -6,7 +6,7 @@ export default function Countries({ countries }) {
       <h1 className="text-3xl font-semibold mb-4">
         Traveling Countries: {countries.length}
       </h1>
-      <div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {countries.map((country, index) => (
           <Country key={index} country={country} />
         ))}
--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,6 +1,6 @@
 export default function Country({ country }) {
   return (
-    <div>
+    <div className="flex flex-col gap-4 border border-amber-300 p-4 rounded-sm">
       <p>
         <span className="font-bold">Name: </span>
         {country.name.common}
@@ -10,6 +10,9 @@ export default function Country({ country }) {
         <span className="font-bold">Independent: </span>
         {country.independent ? "Free" : "Not Free"}
       </p>
+      <button className="bg-gray-400 py-2 w-22 rounded-sm cursor-pointer">
+        Visited
+      </button>
     </div>
   );
 }
